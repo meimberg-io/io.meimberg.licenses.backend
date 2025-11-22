edbd94c3d771df7576e100b0934c569c7cf28e90
OliverMeimberg <oli@meimberg.io>
2025-11-23 00:38:13 +0100

    ide, jdk

---
+++ target/generated-sources/annotations/io/meimberg/licenses/web/mapper/UserMapperImpl.java
@@ -0,0 +1,29 @@
+package io.meimberg.licenses.web.mapper;
+
+import io.meimberg.licenses.web.dto.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-23T00:32:11+0100",
+    comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.8 (Ubuntu)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public User toDto(io.meimberg.licenses.domain.User entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId( entity.getId() );
+        user.setEmail( entity.getEmail() );
+        user.setDisplayName( entity.getDisplayName() );
+
+        return user;
+    }
+}
---
+++ target/generated-sources/openapi/src/main/java/io/meimberg/licenses/web/dto/User.java
@@ -0,0 +1,146 @@
+package io.meimberg.licenses.web.dto;
+
+import java.net.URI;
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import java.util.UUID;
+import org.openapitools.jackson.nullable.JsonNullable;
+import java.time.OffsetDateTime;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.*;
+import org.hibernate.validator.constraints.*;
+import io.swagger.v3.oas.annotations.media.Schema;
+
+
+import java.util.*;
+import jakarta.annotation.Generated;
+
+/**
+ * User
+ */
+
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-11-23T00:32:10.129625873+01:00[Europe/Berlin]", comments = "Generator version: 7.7.0")
+public class User {
+
+  private UUID id;
+
+  private String email;
+
+  private String displayName;
+
+  public User() {
+    super();
+  }
+
+  /**
+   * Constructor with only required parameters
+   */
+  public User(UUID id, String email, String displayName) {
+    this.id = id;
+    this.email = email;
+    this.displayName = displayName;
+  }
+
+  public User id(UUID id) {
+    this.id = id;
+    return this;
+  }
+
+  /**
+   * Get id
+   * @return id
+   */
+  @NotNull @Valid 
+  @Schema(name = "id", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("id")
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public User email(String email) {
+    this.email = email;
+    return this;
+  }
+
+  /**
+   * Get email
+   * @return email
+   */
+  @NotNull @jakarta.validation.constraints.Email 
+  @Schema(name = "email", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("email")
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public User displayName(String displayName) {
+    this.displayName = displayName;
+    return this;
+  }
+
+  /**
+   * Get displayName
+   * @return displayName
+   */
+  @NotNull 
+  @Schema(name = "displayName", requiredMode = Schema.RequiredMode.REQUIRED)
+  @JsonProperty("displayName")
+  public String getDisplayName() {
+    return displayName;
+  }
+
+  public void setDisplayName(String displayName) {
+    this.displayName = displayName;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    User user = (User) o;
+    return Objects.equals(this.id, user.id) &&
+        Objects.equals(this.email, user.email) &&
+        Objects.equals(this.displayName, user.displayName);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, email, displayName);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class User {\n");
+    sb.append("    id: ").append(toIndentedString(id)).append("\n");
+    sb.append("    email: ").append(toIndentedString(email)).append("\n");
+    sb.append("    displayName: ").append(toIndentedString(displayName)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
+
