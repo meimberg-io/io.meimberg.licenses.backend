cc220bb6087535961e989ad2647772b89fdcebe4
OliverMeimberg <oliver.meimberg@gmail.com>
2025-11-23 01:36:53 +0100

    Add CORS configuration and enhance logging in ProductsController and GlobalExceptionHandler

--- target/generated-sources/openapi/src/main/java/io/meimberg/licenses/web/api/AssignmentsApi.java
+++ target/generated-sources/openapi/src/main/java/io/meimberg/licenses/web/api/AssignmentsApi.java
@@ -36,7 +36,7 @@ import java.util.Map;
 import java.util.Optional;
 import jakarta.annotation.Generated;
 
-@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-11-23T00:49:20.868947187+01:00[Europe/Berlin]", comments = "Generator version: 7.7.0")
+@Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2025-11-23T01:28:09.699733449+01:00[Europe/Berlin]", comments = "Generator version: 7.7.0")
 @Validated
 @Tag(name = "assignments", description = "the assignments API")
 public interface AssignmentsApi {
